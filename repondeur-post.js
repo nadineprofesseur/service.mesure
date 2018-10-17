7fadec9c59302dd4cf13705d7ecda3ed159a6012
nadineprofesseur <st-amand.nadine@cgmatane.qc.ca>
2018-10-17 14:36:56 -0400

    Split sur & pour entre les champs

--- repondeur-post.js
+++ repondeur-post.js
@@ -2,16 +2,25 @@ var http = require('http');
 var serveur = http.createServer(
 	function(requete, reponse) 
 	{
-		var data = '';
+		var formulaire = '';
 		requete.on('data', 
-			function(message) 
+			function(champ) 
 			{
-				data += message;
+				formulaire += champ;
+                //console.log('on data ' + champ);
 			}
 		).on('end', 
 			function() 
 			{
-				console.log('POST data: %s', data);
+            
+                listeChamps = formulaire.split('&');
+                for(var position in listeChamps)
+                {
+                    champs = listeChamps[position];
+                    console.log('champs ' + champs);    
+                }
+            
+				console.log('POST data: %s', formulaire);
 			}
 		);
 		
